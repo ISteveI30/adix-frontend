75c6b020348f7d4394c3314bea0b81d4630ea819
Jander Gomez <jandergb.30gmail.com>
2025-03-09 02:29:09 +0100

    feat: add date formatting and currency formatting utilities; create Tailwind CSS configuration

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function formateaFecha(fecha: string): string {
+  return new Date(fecha).toLocaleDateString("es-ES", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  });
+}
+
+export const formatearMoneda = (monto: number) => {
+  if (isNaN(monto)) monto = 0;
+  return new Intl.NumberFormat("es-PE", {
+    style: "currency",
+    currency: "PEN",
+  }).format(monto);
+};
