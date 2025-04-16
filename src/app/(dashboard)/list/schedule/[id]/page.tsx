6dc1af245425b210ae1f164b2907f5ccf66c4054
Jander Gomez <jandergb.30gmail.com>
2025-04-16 03:35:25 +0200

    feat: create AccountReceivableListPage and ScheduleTable components for managing account receivables with data fetching and rendering

---
+++ src/app/(dashboard)/list/schedule/[id]/page.tsx
@@ -0,0 +1,29 @@
+// "use client";
+
+import { Suspense} from 'react';
+import ScheduleTable from '../scheduleTable';
+
+
+  const AccountReceivableListPage =  async({
+    params,
+  }: {
+    params: { id: string };
+  }) => {
+  
+  const { id } = await params;
+
+  return (
+    <div className="flex flex-col gap-4 w-full p-4">
+      <h1 className="text-2xl font-bold">Cuentas por cobrar</h1>
+
+      <div className="overflow-x-auto">
+        <Suspense key={id} fallback={<div className="p-4 text-center">Cargando tabla...</div>}>
+          <ScheduleTable id={id} />
+        </Suspense>
+      </div>
+
+    </div>
+  );
+};
+
+export default AccountReceivableListPage;
---
+++ src/app/(dashboard)/list/schedule/scheduleTable.tsx
@@ -0,0 +1,113 @@
+"use client"
+
+import { AccountReceivable } from "@/api/interfaces/account-receivable.interface";
+import { AccountReceivableService } from "@/api/models/accountReceivable/account-receivable.api";
+import PaymentStatus from "@/components/customs/status";
+import TableView, { ColumnDefinition } from "@/components/customs/TableView";
+import { Button } from "@/components/ui/button";
+import { ROLE } from "@/lib/data";
+import { formatCurrency } from "@/lib/utils";
+import { Calendar1Icon } from "lucide-react";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
+const columns: ColumnDefinition<AccountReceivable>[] = [
+  { header: "Estado de Pago", accessor: "status" },
+  { header: "Monto total", accessor: "totalAmount", className: "hidden md:table-cell" },
+  { header: "Balance", accessor: "pendingBalance", className: "hidden md:table-cell" },
+  { header: "Concepto", accessor: "concept", className: "hidden lg:table-cell" },
+  { header: "Fecha de Vencimiento", accessor: "dueDate", className: "hidden md:table-cell" },
+  { header: "Acciones", accessor: "actions" },
+];
+
+
+export default function ScheduleTable({
+  id
+}: {
+  id: string;
+}) {
+  const [dataAccount, setDataAccount] = useState<AccountReceivable[]>([]);
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+
+      try {
+        const data = await AccountReceivableService.listAccountReceivablesByStudent(id);
+
+        if (!data || data.length === 0) {
+          setDataAccount([]);
+          console.info("No se encontraron datos de matrícula.");
+          return;
+        }
+
+        const transformed = data.map((item): AccountReceivable => ({
+          id: item.id,
+          status: item.status,
+          totalAmount: item.totalAmount,
+          pendingBalance: item.pendingBalance,
+          concept: item.concept,
+          dueDate: new Date(item.dueDate).toLocaleDateString(),
+        }));
+        setDataAccount(transformed);
+      } catch (error) {
+        console.error("Error al cargar los datos:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [id]);
+
+  const renderRow = (item: AccountReceivable) => {
+
+    return (
+      <tr key={item.id} className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-purple-400  cursor-pointer">
+        <td className="p-3"><PaymentStatus status={item.status} /></td>
+        <td className="p-3 hidden md:table-cell">{formatCurrency(item.totalAmount)}</td>
+        <td className="p-3 hidden md:table-cell">{formatCurrency(item.pendingBalance)}</td>
+        <td className="p-3 hidden lg:table-cell">{item.concept}</td>
+        <td className="p-3 hidden md:table-cell">{item.dueDate}</td>
+
+        <td>
+          {ROLE === "admin" && (
+            <button
+              title="Eliminar"
+              // onClick={() => handleDelete(item.enrollmentId!)}
+              className="w-7 h-7 flex items-center justify-center rounded-full bg-green-500 text-white cursor-pointer"
+            >
+              <Calendar1Icon size={16} />
+            </button>
+          )}
+        </td>
+      </tr>
+    )
+  };
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
+  return (
+    <>
+      <div className="flex items-center justify-around mb-4 ">
+        <Link href={`/list/enrollments`} title="Regresar a la lista de matrículas">
+          <Button className="w-auto p-2 flex items-center justify-center rounded-full bg-blue-500 text-white cursor-pointer">
+            Regresar a la lista de matrículas
+          </Button>
+        </Link>
+      </div>
+      <div className="overflow-x-auto mb-4">
+        <p className="text-sm text-gray-500">Total de cuentas por cobrar: {dataAccount.length}</p>
+      </div>
+      <TableView
+        columns={columns}
+        renderRow={renderRow}
+        data={dataAccount}
+      />
+    </>
+  );
+}
