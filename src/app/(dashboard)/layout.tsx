fde58881870b78b88aba53d5c91ca04d2bde40c8
Jander Gomez <jandergb.30gmail.com>
2025-04-16 03:36:57 +0200

    feat: optimize DashboardLayout with cached components and add Toaster for notifications

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,10 @@
 import Menu from "@/components/customs/Menu";
 import Navbar from "@/components/customs/Navbar";
+import { Toaster } from "@/components/ui/sonner"
+import { cache } from 'react';
+
+const CachedNavbar = cache(() => <Navbar />);
+const CachedMenu = cache(() => <Menu />);
 
 export default function DashboardLayout({
   children,
@@ -8,13 +13,14 @@ export default function DashboardLayout({
 }>) {
   return (
     <>
-    <Navbar />
+    <CachedNavbar />
     <section className="h-screen flex">
       <article className="w-[14%] md:w-[8%] lg:w-[16%]">
-        <Menu />
+        <CachedMenu />
       </article>
       <article className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
         {children}
+        <Toaster />
       </article>
     </section>
     </>
