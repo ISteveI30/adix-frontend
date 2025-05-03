1386659fd986a28c632dee065e6b1b64eec271a8
Jander Gomez <jandergb.30gmail.com>
2025-05-03 16:41:29 +0200

    feat: replace TutorEditForm with TutorForm in TutorEditPage

--- src/app/(dashboard)/list/tutors/[id]/page.tsx
+++ src/app/(dashboard)/list/tutors/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { Tutor } from '@/api/interfaces/tutor.interface'
 import { TutorService } from '@/api/models/tutor/tutor.api'
-import TutorEditForm from '@/components/forms/TutorEditForm'
+import TutorForm from '@/components/forms/TutorForm';
 import { Suspense, use } from 'react'
 
 interface TutorEditPageProps {
@@ -14,18 +14,16 @@ interface TutorEditPageProps {
 
 const TutorEditPage = ({ params, searchParams }: TutorEditPageProps) => {
   const { id } = use(params)
-  
   const { page } = use(searchParams)
-
   const tutorData: Tutor = use(TutorService.getTutorById(id))
 
   return (
     <div className="flex flex-col gap-4 w-full p-6 bg-white rounded-lg shadow-sm">
       <h2 className="text-2xlg font-bold text-gray-800">Editar Datos de Apoderado</h2>
       <Suspense fallback={<div className="p-4 text-center">Cargando Datos del Apoderado...</div>}>
-        <TutorEditForm
+        <TutorForm
           data={tutorData}
-          page={page ? Number(page) : 1}
+          page={Number(page)}
         />
       </Suspense>
     </div>
